d6e3feb2fc4de6d8aba0694eee6450d93474e042
xuecl <xuecl4@gmail.com>
2021-08-06 20:46:23 +0800

    1

--- src/service/area.js
+++ src/service/area.js
@@ -4,14 +4,14 @@ import utils from '../utils/service-utils.js'
 
 const moduleCode = '002'
 
-function getCurrentArea(army) {
+export function getCurrentArea(army) {
     const coordinate = army.area.coordinate
     const area = map.getArea(coordinate)
     if(!area) return utils.getBusFailureRsp(moduleCode + '001', '不存在的坐标')
     return utils.getSuccessRsp(new areaInfoApi(area))
 }
 
-function moveTo(sessionId, direction) {
+export function moveTo(sessionId, direction) {
     const area = map.getArea(coordinate)
     if(!area) return utils.getBusFailureRsp(moduleCode + '001', '不存在的坐标')
     const destiny = area.moveTo(direction)
