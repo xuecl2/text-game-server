8cc1cb443dbf36b1cf9540d448ce49d25b8d6123
xuecl <xuecl2@163.com>
2021-08-17 20:39:58 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import { sessions, armies } from '../src/global/global.js'
 import { login, regist } from '../src/service/account.js'
 import { getCurrentArea, moveTo } from '../src/service/area.js'
+import { getArmyInfo } from '../src/service/army.js'
 
 export default {
   login,
   regist,
   'get-current-area': getServlet(getCurrentArea), 
   'move-to': getServlet(moveTo),
+  'get-army-info': getServlet(getArmyInfo),
 }
 
 function getServlet(func) {
