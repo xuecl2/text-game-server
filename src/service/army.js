4800edfd2f68cf9cff5b2134bffb065705a2bdf5
xuecl <xuecl4@gmail.com>
2021-08-17 21:52:43 +0800

    1

--- src/service/army.js
+++ src/service/army.js
@@ -1,10 +1,11 @@
 import { armies } from "../global/global.js"
 import utils from '../utils/service-utils.js' 
+import { armyInfoApi } from '../apis/army-api.js'
 
 const moduleCode = '003'
 
 export function getArmyInfo(req) {
     const army = armies.get(req.id)
     if(!army || !army.mainHero) return utils.getBusFailureRsp(moduleCode + '001', '不存在的军团')
-    return utils.getSuccessRsp(new armyInfo(destiny))
+    return utils.getSuccessRsp(new armyInfoApi(army))
 }
