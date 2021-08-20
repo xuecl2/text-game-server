a19e96e717dd62b3e258ba9e474aeccf1f04127b
xuecl <xuecl2@163.com>
2021-08-20 13:03:03 +0800

    1

--- src/service/hero.js
+++ src/service/hero.js
@@ -7,8 +7,9 @@ const moduleCode = '004'
 export function hireHero(req, army) {   
     const heroId = req.heroId
     let hero = heros.get(heroId)
-    if(!hero) return utils.getBusFailureRsp(moduleCode + '001', '不存在的英雄！')
+    if(!hero) return utils.getBusFailureRsp(moduleCode + '001', '不存在的佣兵！')
     army.hireHero(hero)
+    return utils.getSuccessRsp()
 } 
 
 export function registMainHeor(req, army) {
@@ -18,4 +19,11 @@ export function registMainHeor(req, army) {
     army.hireHero(hero)
     army.setBattle(hero)
     if(!heroClassName || !gender) return utils.getBusFailureRsp(moduleCode + '002', '职业或性别不能为空')
+}
+
+export function getHeroInfo(req, army) {
+    const heroId = req.heroId
+    const hero = heros.get(heroId)
+    if(!hero) return utils.getBusFailureRsp(moduleCode + '003', '佣兵不存在！')
+    return utils.getSuccessRsp(new heroInfoApi(hero))
 }
