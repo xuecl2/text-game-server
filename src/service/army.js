5929a6dd2a21ff7d23d40551e8e975f9c41b09ec
xuecl <xuecl4@gmail.com>
2021-11-05 22:36:49 +0800

    1. gitignore 增加nohup 2. 查询军团接口增加判空

--- src/service/army.js
+++ src/service/army.js
@@ -5,7 +5,8 @@ import { armyInfoApi } from '../apis/army-api.js'
 const moduleCode = '003'
 
 export function getArmyInfo(req) {
+    if(!req.id) return utils.getBusFailureRsp(moduleCode + '001', '军团id不能为空!')
     const army = armies.get(req.id)
-    if(!army || !army.mainHero) return utils.getBusFailureRsp(moduleCode + '001', '不存在的军团')
+    if(!army?.mainHero) return utils.getBusFailureRsp(moduleCode + '002', '不存在的军团')
     return utils.getSuccessRsp(new armyInfoApi(army))
 }
